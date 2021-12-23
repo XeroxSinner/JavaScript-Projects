8afac195486126ba86e5f013bb24360c47c4f235
Ny <nyarce805@gmail.com>
2021-12-23 14:49:51 -0600

    Complete Tic Tac Toe Assignment

---
+++ Basic JavaScript Projects/Project11_Switch/JS/form.js
@@ -0,0 +1,11 @@
+function setup(){
+    validateForm()
+}
+
+function validateForm() {
+    var x = document.forms["myform"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  } 
